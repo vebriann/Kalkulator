c27892f568b0b1612ff3d9591ba458a34c2ac4e8
vebriann <vebrianns01@gmail.com>
2023-09-03 13:07:22 +0700

    kalkulator

---
+++ Kalkulator/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.satu = new System.Windows.Forms.Button();
+            this.dua = new System.Windows.Forms.Button();
+            this.tiga = new System.Windows.Forms.Button();
+            this.enam = new System.Windows.Forms.Button();
+            this.lima = new System.Windows.Forms.Button();
+            this.empat = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.sembilan = new System.Windows.Forms.Button();
+            this.delapan = new System.Windows.Forms.Button();
+            this.tujuh = new System.Windows.Forms.Button();
+            this.enol = new System.Windows.Forms.Button();
+            this.kali = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.angkaX = new System.Windows.Forms.Label();
+            this.aritmatika = new System.Windows.Forms.Label();
+            this.angkaY = new System.Windows.Forms.Label();
+            this.hasiloperasi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // satu
+            // 
+            this.satu.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.satu.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.satu.Location = new System.Drawing.Point(45, 128);
+            this.satu.Name = "satu";
+            this.satu.Size = new System.Drawing.Size(66, 67);
+            this.satu.TabIndex = 1;
+            this.satu.Text = "1";
+            this.satu.UseVisualStyleBackColor = true;
+            this.satu.Click += new System.EventHandler(this.satu_Click);
+            // 
+            // dua
+            // 
+            this.dua.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dua.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dua.Location = new System.Drawing.Point(134, 128);
+            this.dua.Name = "dua";
+            this.dua.Size = new System.Drawing.Size(66, 67);
+            this.dua.TabIndex = 2;
+            this.dua.Text = "2";
+            this.dua.UseVisualStyleBackColor = true;
+            this.dua.Click += new System.EventHandler(this.dua_Click);
+            // 
+            // tiga
+            // 
+            this.tiga.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tiga.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tiga.Location = new System.Drawing.Point(221, 128);
+            this.tiga.Name = "tiga";
+            this.tiga.Size = new System.Drawing.Size(66, 67);
+            this.tiga.TabIndex = 3;
+            this.tiga.Text = "3";
+            this.tiga.UseVisualStyleBackColor = true;
+            this.tiga.Click += new System.EventHandler(this.tiga_Click);
+            // 
+            // enam
+            // 
+            this.enam.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.enam.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.enam.Location = new System.Drawing.Point(221, 218);
+            this.enam.Name = "enam";
+            this.enam.Size = new System.Drawing.Size(66, 67);
+            this.enam.TabIndex = 6;
+            this.enam.Text = "6";
+            this.enam.UseVisualStyleBackColor = true;
+            this.enam.Click += new System.EventHandler(this.enam_Click);
+            // 
+            // lima
+            // 
+            this.lima.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lima.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lima.Location = new System.Drawing.Point(134, 218);
+            this.lima.Name = "lima";
+            this.lima.Size = new System.Drawing.Size(66, 67);
+            this.lima.TabIndex = 5;
+            this.lima.Text = "5";
+            this.lima.UseVisualStyleBackColor = true;
+            this.lima.Click += new System.EventHandler(this.lima_Click);
+            // 
+            // empat
+            // 
+            this.empat.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.empat.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.empat.Location = new System.Drawing.Point(45, 218);
+            this.empat.Name = "empat";
+            this.empat.Size = new System.Drawing.Size(66, 67);
+            this.empat.TabIndex = 4;
+            this.empat.Text = "4";
+            this.empat.UseVisualStyleBackColor = true;
+            this.empat.Click += new System.EventHandler(this.empat_Click);
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.start.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.start.Location = new System.Drawing.Point(411, 309);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(88, 67);
+            this.start.TabIndex = 7;
+            this.start.Text = "=";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clear.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.clear.Location = new System.Drawing.Point(308, 218);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(66, 67);
+            this.clear.TabIndex = 8;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.plus.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.plus.Location = new System.Drawing.Point(308, 128);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(66, 67);
+            this.plus.TabIndex = 9;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // min
+            // 
+            this.min.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.min.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.min.Location = new System.Drawing.Point(411, 218);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(88, 67);
+            this.min.TabIndex = 10;
+            this.min.Text = "-";
+            this.min.UseVisualStyleBackColor = true;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // sembilan
+            // 
+            this.sembilan.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sembilan.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sembilan.Location = new System.Drawing.Point(221, 309);
+            this.sembilan.Name = "sembilan";
+            this.sembilan.Size = new System.Drawing.Size(66, 67);
+            this.sembilan.TabIndex = 13;
+            this.sembilan.Text = "9";
+            this.sembilan.UseVisualStyleBackColor = true;
+            this.sembilan.Click += new System.EventHandler(this.sembilan_Click);
+            // 
+            // delapan
+            // 
+            this.delapan.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.delapan.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.delapan.Location = new System.Drawing.Point(134, 309);
+            this.delapan.Name = "delapan";
+            this.delapan.Size = new System.Drawing.Size(66, 67);
+            this.delapan.TabIndex = 12;
+            this.delapan.Text = "8";
+            this.delapan.UseVisualStyleBackColor = true;
+            this.delapan.Click += new System.EventHandler(this.delapan_Click);
+            // 
+            // tujuh
+            // 
+            this.tujuh.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tujuh.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tujuh.Location = new System.Drawing.Point(45, 309);
+            this.tujuh.Name = "tujuh";
+            this.tujuh.Size = new System.Drawing.Size(66, 67);
+            this.tujuh.TabIndex = 11;
+            this.tujuh.Text = "7";
+            this.tujuh.UseVisualStyleBackColor = true;
+            this.tujuh.Click += new System.EventHandler(this.tujuh_Click);
+            // 
+            // enol
+            // 
+            this.enol.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.enol.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.enol.Location = new System.Drawing.Point(308, 309);
+            this.enol.Name = "enol";
+            this.enol.Size = new System.Drawing.Size(66, 67);
+            this.enol.TabIndex = 14;
+            this.enol.Text = "0";
+            this.enol.UseVisualStyleBackColor = true;
+            this.enol.Click += new System.EventHandler(this.enol_Click);
+            // 
+            // kali
+            // 
+            this.kali.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.kali.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.kali.Location = new System.Drawing.Point(411, 128);
+            this.kali.Name = "kali";
+            this.kali.Size = new System.Drawing.Size(88, 67);
+            this.kali.TabIndex = 15;
+            this.kali.Text = "x";
+            this.kali.UseVisualStyleBackColor = true;
+            this.kali.Click += new System.EventHandler(this.kali_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Kalkulator.Properties.Resources.Rectangle_34__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(454, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // angkaX
+            // 
+            this.angkaX.BackColor = System.Drawing.Color.White;
+            this.angkaX.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.angkaX.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.angkaX.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.angkaX.Location = new System.Drawing.Point(55, 46);
+            this.angkaX.Name = "angkaX";
+            this.angkaX.Size = new System.Drawing.Size(408, 57);
+            this.angkaX.TabIndex = 17;
+            this.angkaX.Text = "0";
+            this.angkaX.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.angkaX.Click += new System.EventHandler(this.angkaX_Click);
+            // 
+            // aritmatika
+            // 
+            this.aritmatika.BackColor = System.Drawing.Color.White;
+            this.aritmatika.Font = new System.Drawing.Font("Adobe Devanagari", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.aritmatika.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.aritmatika.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.aritmatika.Location = new System.Drawing.Point(469, 46);
+            this.aritmatika.Name = "aritmatika";
+            this.aritmatika.Size = new System.Drawing.Size(20, 27);
+            this.aritmatika.TabIndex = 18;
+            this.aritmatika.Text = "+";
+            this.aritmatika.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // angkaY
+            // 
+            this.angkaY.BackColor = System.Drawing.Color.White;
+            this.angkaY.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.angkaY.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.angkaY.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.angkaY.Location = new System.Drawing.Point(55, 46);
+            this.angkaY.Name = "angkaY";
+            this.angkaY.Size = new System.Drawing.Size(408, 57);
+            this.angkaY.TabIndex = 19;
+            this.angkaY.Text = "0";
+            this.angkaY.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.angkaY.Click += new System.EventHandler(this.angkaY_Click);
+            // 
+            // hasiloperasi
+            // 
+            this.hasiloperasi.BackColor = System.Drawing.Color.White;
+            this.hasiloperasi.Font = new System.Drawing.Font("Adobe Devanagari", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.hasiloperasi.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.hasiloperasi.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.hasiloperasi.Location = new System.Drawing.Point(55, 46);
+            this.hasiloperasi.Name = "hasiloperasi";
+            this.hasiloperasi.Size = new System.Drawing.Size(408, 57);
+            this.hasiloperasi.TabIndex = 20;
+            this.hasiloperasi.Text = "=";
+            this.hasiloperasi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.hasiloperasi.Click += new System.EventHandler(this.hasiloperasi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hasiloperasi);
+            this.Controls.Add(this.angkaY);
+            this.Controls.Add(this.aritmatika);
+            this.Controls.Add(this.angkaX);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.kali);
+            this.Controls.Add(this.enol);
+            this.Controls.Add(this.sembilan);
+            this.Controls.Add(this.delapan);
+            this.Controls.Add(this.tujuh);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.enam);
+            this.Controls.Add(this.lima);
+            this.Controls.Add(this.empat);
+            this.Controls.Add(this.tiga);
+            this.Controls.Add(this.dua);
+            this.Controls.Add(this.satu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button satu;
+        private Button dua;
+        private Button tiga;
+        private Button enam;
+        private Button lima;
+        private Button empat;
+        private Button start;
+        private Button clear;
+        private Button plus;
+        private Button min;
+        private Button sembilan;
+        private Button delapan;
+        private Button tujuh;
+        private Button enol;
+        private Button kali;
+        private PictureBox pictureBox1;
+        private Label angkaX;
+        private Label angkaY;
+        private Label aritmatika;
+        private Label hasiloperasi;
+    }
+}
